67b6cede1c7aeb87649baf614d320df8e785fd01
Eduard Knezovic <knezovic.eduard@gmail.com>
2025-02-20 16:51:58 +0100

    do the hello world in java

--- FlashSaleTest.java
+++ FlashSaleTest.java
@@ -1,7 +1,7 @@
-import org.testcontainers.containers.ScyllaDBContainer;
+// import org.testcontainers.containers.ScyllaDBContainer;
 
 public class FlashSaleTest {
-    static ScyllaDBContainer scylladb = new ScyllaDBContainer("scylladb/scylla:6.2");
+    // static ScyllaDBContainer scylladb = new ScyllaDBContainer("scylladb/scylla:6.2");
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
