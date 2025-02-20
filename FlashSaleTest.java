582b8028f912a61e9fb49bb3c9d18128f9f0ec14
Eduard Knezovic <knezovic.eduard@gmail.com>
2025-02-20 16:47:41 +0100

    gradle build succesful

--- FlashSaleTest.java
+++ FlashSaleTest.java
@@ -1,5 +1,8 @@
+import org.testcontainers.containers.ScyllaDBContainer;
 
 public class FlashSaleTest {
+    static ScyllaDBContainer scylladb = new ScyllaDBContainer("scylladb/scylla:6.2");
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
